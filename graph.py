786ba1c016e4d668644fe22a9ff9d7f8644721b2
JinZhou5042 <jzhou24@nd.edu>
2024-07-22 17:20:40 -0400

    multi processin

--- graph.py
+++ graph.py
@@ -34,10 +34,10 @@ class OrthogonalListGraph:
     def __init__(self):
         self.vertices = {}
         self.edges = {}
-        self.components = []
+        self.subgraphs = []
 
-    def get_num_of_components(self):
-        return len(self.components)
+    def get_num_of_subgraphs(self):
+        return len(self.subgraphs)
 
     def add_vertex(self, task_id, task_life_time=0):
         if task_id not in self.vertices:
@@ -72,46 +72,46 @@ class OrthogonalListGraph:
                 print(f"  In from {in_edge.tail}")
                 in_edge = in_edge.head_link
 
-    def update_components(self):
+    def update_subgraphs(self):
         visited = set()
-        self.components = []
+        self.subgraphs = []
 
-        def dfs(v, component):
+        def dfs(v, subgraph):
             visited.add(v)
-            component.append(v)
+            subgraph.append(v)
             edge = self.vertices[v].first_out
             while edge:
                 if edge.head not in visited:
-                    dfs(edge.head, component)
+                    dfs(edge.head, subgraph)
                 edge = edge.tail_link
             edge = self.vertices[v].first_in
             while edge:
                 if edge.tail not in visited:
-                    dfs(edge.tail, component)
+                    dfs(edge.tail, subgraph)
                 edge = edge.head_link
 
         for vertex in self.vertices:
             if vertex not in visited:
-                component = []
-                dfs(vertex, component)
-                self.components.append(component)
+                subgraph = []
+                dfs(vertex, subgraph)
+                self.subgraphs.append(subgraph)
 
-    def find_critical_path_in_component(self, component):
-        in_degree = {v: 0 for v in component}
-        longest_path = {v: 0 for v in component}
-        predecessor = {v: None for v in component}
+    def find_critical_path_in_subgraph(self, subgraph):
+        in_degree = {v: 0 for v in subgraph}
+        longest_path = {v: 0 for v in subgraph}
+        predecessor = {v: None for v in subgraph}
 
         # Calculate in-degrees
-        for v in component:
+        for v in subgraph:
             vertex = self.vertices[v]
             edge = vertex.first_out
             while edge:
-                if edge.head in component:
+                if edge.head in subgraph:
                     in_degree[edge.head] += 1
                 edge = edge.tail_link
 
         # Initialize queue with vertices having zero in-degree
-        queue = deque([v for v in component if in_degree[v] == 0])
+        queue = deque([v for v in subgraph if in_degree[v] == 0])
         
         # Topological order and longest path calculation
         topo_order = []
@@ -120,7 +120,7 @@ class OrthogonalListGraph:
             topo_order.append(v)
             edge = self.vertices[v].first_out
             while edge:
-                if edge.head in component:
+                if edge.head in subgraph:
                     if longest_path[edge.head] < longest_path[v] + self.vertices[edge.head].task_life_time + edge.weight:
                         longest_path[edge.head] = longest_path[v] + self.vertices[edge.head].task_life_time + edge.weight
                         predecessor[edge.head] = v
@@ -143,85 +143,91 @@ class OrthogonalListGraph:
         critical_path.reverse()
         return critical_path
     
-    def plot_component(self, component, view=False, save_to=None):
+    def plot_subgraph(self, subgraph, view=False, save_to=None):
         if not save_to:
             print("Error: save_to is not provided.")
             return
         dot = graphviz.Digraph()
         
         # create nodes, each node is a task
-        for task_id in component:
-            dot.node(str(task_id), str(task_id))
-            this_task = task_done_df[task_done_df['task_id'] == task_id].iloc[0]
+        for task_id in subgraph:
+            if args.task_node_label == 'task-id':
+                task_node_label = str(task_id)
+            elif args.task_node_label == 'graph-id':
+                task_node_label = str(task_id) + f"{task_info[task_id]['graph_id']}"
+            dot.node(str(task_id), task_node_label, shape='ellipse')
+            this_task = task_info[task_id]
             # highlight recovery tasks
             if this_task['is_recovery_task']:
-                dot.node(str(task_id), str(task_id), style='filled', color='#ea67a9', shape='ellipse')
+                dot.node(str(task_id), task_node_label, style='filled', color='#ea67a9', shape='ellipse')
             else:
-                dot.node(str(task_id), str(task_id), shape='ellipse')
+                dot.node(str(task_id), task_node_label, shape='ellipse')
 
             if args.no_files:
                 # plot edges from this task to its successors
                 edge = self.vertices[task_id].first_out
                 while edge:
-                    label = f"{edge.weight}s" if not args.no_weight else None
-                    dot.edge(str(task_id), str(edge.head), label=label)
+                    edge_label = f"{edge.weight}s" if not args.no_weight else None
+                    dot.edge(str(task_id), str(edge.head), label=edge_label)
                     edge = edge.tail_link
             else:
                 # plot edges from input files to this task
-                for input_file in task_done_df[task_done_df['task_id'] == task_id]['input_files'].values[0]:
+                for input_file in task_info[task_id]['input_files']:
                     file = general_statistics_file_df[general_statistics_file_df['filename'] == input_file].iloc[0]
                     actual_producer_task_id = 0
                     actual_producer_task = None
                     for producer_task_id in file['producers']:
                         # the producers are already sorted by time
-                        producer_task = task_done_df[task_done_df['task_id'] == producer_task_id].iloc[0]
+                        producer_task = task_info[producer_task_id]
                         if float(producer_task[task_finish_timestamp]) <= float(this_task[task_start_timestamp]):
                             actual_producer_task_id = producer_task_id
                             actual_producer_task = producer_task
                     if actual_producer_task is None:
                         print(f"Warning: Task {task_id} has no producer task for input file {input_file}.")
                     time_period = round(float(this_task[task_start_timestamp]) - float(actual_producer_task[task_finish_timestamp]), 4)
-                    label = f"{time_period}s" if not args.no_weight else None
+                    edge_label = f"{time_period}s" if not args.no_weight else None
                     if time_period < 0:
                         # it means that this input file is lost after this task is done and it is used as another task's input file                        
                         print(f"Warning: Task {task_id} is started before its producer task {actual_producer_task_id} is finished.")
                         continue
                     dot.node(input_file, input_file, shape='box')
                     if this_task['is_recovery_task'] or actual_producer_task['is_recovery_task']:
-                        dot.edge(input_file, str(task_id), color='#ea67a9', style='dashed', label=label)
+                        dot.edge(input_file, str(task_id), color='#ea67a9', style='dashed', label=edge_label)
                     else:
-                        dot.edge(input_file, str(task_id), label=label)
+                        dot.edge(input_file, str(task_id), label=edge_label)
                 # plot edges from this task to output files
-                for output_file in task_done_df[task_done_df['task_id'] == task_id]['output_files'].values[0]:
+                for output_file in task_info[task_id]['output_files']:
                     time_period = round(float(this_task[task_finish_timestamp]) - float(this_task[task_start_timestamp]), 4)
-                    label = f"{time_period}s" if not args.no_weight else None
+                    edge_label = f"{time_period}s" if not args.no_weight else None
                     dot.node(output_file, output_file, shape='box')
                     if this_task['is_recovery_task']:
-                        dot.edge(str(task_id), output_file, label=label, color='#ea67a9', style='dashed')
+                        dot.edge(str(task_id), output_file, label=edge_label, color='#ea67a9', style='dashed')
                     else:
-                        dot.edge(str(task_id), output_file, label=label)
+                        dot.edge(str(task_id), output_file, label=edge_label)
 
         dot.attr(rankdir='TB')
         dot.render(save_to, format='svg', view=view)
 
 
-def process_component(args):
-    graph, component, graph_id = args
-    task_done_df.loc[task_done_df['task_id'].isin(component), 'graph_id'] = graph_id
+def process_subgraph(args):
+    graph, subgraph, graph_id = args
 
-    graph.plot_component(component, save_to=os.path.join(dirname, f"subgraph_{graph_id}"), view=False)
-    root = component[0]
+    for task_id in subgraph:
+        task_info[task_id]['graph_id'] = graph_id
+
+    graph.plot_subgraph(subgraph, save_to=os.path.join(dirname, f"subgraph_{graph_id}"), view=False)
+    root = subgraph[0]
     graph_info = {
         'graph_id': graph_id,
-        'num_tasks': len(component),
+        'num_tasks': len(subgraph),
         'num_critical_tasks': 0,
         'critical_tasks': 0,
         'time_critical_nodes': [],
         'time_critical_edges': [],
         'time_critical_path': 0,
-        'tasks': component,
+        'tasks': subgraph,
     }
-    graph_info['critical_tasks'] = graph.find_critical_path_in_component(component)
+    graph_info['critical_tasks'] = graph.find_critical_path_in_subgraph(subgraph)
     graph_info['num_critical_tasks'] = len(graph_info['critical_tasks'])
     for i, task_id in enumerate(graph_info['critical_tasks']):
         graph_info['time_critical_nodes'].append(graph.vertices[task_id].task_life_time)
@@ -232,48 +238,44 @@ def process_component(args):
 
     return root, graph_info
 
+def generate_subgraphs(graph):
+    print(f"Processing subgraphs with {cpu_count()} cores...")
+    graph_info = {}
+
+    pbar = tqdm.tqdm(total=len(graph.subgraphs))
+    with Pool(cpu_count()) as pool:
+        results = pool.imap_unordered(process_subgraph, [(graph, subgraph, i + 1) for i, subgraph in enumerate(graph.subgraphs)])
+        for root, info in results:
+            graph_info[root] = info
+            pbar.update(1)
+    pbar.close()
+
+    return graph_info
+
 
 def generate_graph():
     print("Generating graph...")
     graph = OrthogonalListGraph()
     input_to_tasks = {}
 
-    task_done_list = task_done_df.to_dict('records')
-
-    for task in task_done_list:
+    for task_id, task in task_info.items():
         execution_time = round(float(task[task_finish_timestamp]) - float(task[task_start_timestamp]), 4)
-        graph.add_vertex(task['task_id'], execution_time)
+        graph.add_vertex(task_id, execution_time)
         for file in task['input_files']:
-            input_to_tasks.setdefault(file, []).append(task['task_id'])
+            input_to_tasks.setdefault(file, []).append(task_id)
 
-    for task in task_done_list:
+    for task_id, task in task_info.items():
         for file in task['output_files']:
             if file in input_to_tasks:
                 tail_task = task
                 for target_task_id in input_to_tasks[file]:
-                    head_task = task_done_df[task_done_df['task_id'] == target_task_id].iloc[0]
+                    head_task = task_info[target_task_id]
                     weight = round(float(head_task[task_start_timestamp]) - float(tail_task[task_finish_timestamp]), 4)
-                    graph.add_edge(task['task_id'], target_task_id, weight=weight)
+                    graph.add_edge(task_id, target_task_id, weight=weight)
 
-    graph.update_components()
+    graph.update_subgraphs()
 
     return graph
-
-def generate_sub_graphs(graph):
-    print(f"Processing components with {cpu_count()} cores...")
-    graph_info = {}
-
-    pbar = tqdm.tqdm(total=len(graph.components))
-    with Pool(cpu_count()) as pool:
-        results = pool.imap_unordered(process_component, [(graph, component, i + 1) for i, component in enumerate(graph.components)])
-        for root, info in results:
-            graph_info[root] = info
-            pbar.update(1)    
-    pbar.close()
-
-    graph_info_df = pd.DataFrame.from_dict(graph_info, orient='index')
-    graph_info_df.to_csv(os.path.join(dirname, 'graph_info.csv'), index=False)
-    task_done_df.to_csv(os.path.join(dirname, 'task_done.csv'), index=False)
 
 
 if __name__ == '__main__':
@@ -283,6 +285,7 @@ if __name__ == '__main__':
     parser.add_argument('log_dir', type=str, help='the target log directory')
     parser.add_argument('--no-files', action='store_true')
     parser.add_argument('--no-weight', action='store_true')
+    parser.add_argument('--task-node-label', type=str, default='task-id')
     args = parser.parse_args()
 
     task_start_timestamp = 'time_worker_start'
@@ -294,10 +297,16 @@ if __name__ == '__main__':
     task_done_df = pd.read_csv(os.path.join(dirname, 'task_done.csv'))
     task_done_df['input_files'] = task_done_df['input_files'].apply(safe_literal_eval)
     task_done_df['output_files'] = task_done_df['output_files'].apply(safe_literal_eval)
+    task_info = task_done_df.set_index('task_id', inplace=False).to_dict('index') 
 
     general_statistics_file_df = pd.read_csv(os.path.join(dirname, 'general_statistics_file.csv'))
     general_statistics_file_df['producers'] = general_statistics_file_df['producers'].apply(safe_literal_eval)
     general_statistics_file_df['consumers'] = general_statistics_file_df['consumers'].apply(safe_literal_eval)
     
     graph = generate_graph()
-    generate_sub_graphs(graph)
+    graph_info = generate_subgraphs(graph)
+
+    graph_info_df = pd.DataFrame.from_dict(graph_info, orient='index')
+    graph_info_df.to_csv(os.path.join(dirname, 'graph_info.csv'), index=False)
+    task_done_df = pd.DataFrame.from_dict(task_info, orient='index')
+    task_done_df.to_csv(os.path.join(dirname, 'task_done.csv'), index=False)
